f12f89d18de2a9d719d3c88372c3deb17a36d0db
Gibexz <andrewetedu@gmail.com>
2023-01-24 14:32:03 +0000

    Updates for Task 3

--- print_base8_o.c
+++ print_base8_o.c
@@ -4,29 +4,29 @@
  * print_base8_o - funtion to print number to base 8
  * @ap: va_list variable
  *
- * Return: Count
+ * Return: count
  */
 int print_base8_o(va_list ap)
 {
 	unsigned int digit[12];
 	unsigned int i, sum_digit;
 	int count = 0;
-	unsigned int num, m;
+	unsigned long int num, m;
 
-	num = va_arg(ap, unsigned int);
+	num = (unsigned long int)va_arg(ap, unsigned int);
 	sum_digit = 0;
-	m = 1073741824; /* 8 ^ 10 */
+	m = 8589934592; /* 8 ^ 11 */
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 12; i++)
 	{
-		digit[i] = (num / m) % 8;
+		digit[i] = (unsigned int)((num / m) % 8);
 		m = m / 8;
 	}
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 12; i++)
 	{
 		sum_digit += digit[i];
-		if (sum_digit || i == 11)
+		if (sum_digit != 0 || i == 11)
 		{
 			_putchar(digit[i]);
 			count++;
